b040aba61417e7e8a9a0d002d363ebc4250cd337
kascez <zorankascelan60@gmail.com>
2019-09-18 17:28:32 +0200

    stilske promjene

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("resources/view/firstStartScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("resources/view/userInfo.fxml"));
         primaryStage.setTitle("FKevin");
         primaryStage.setFullScreenExitHint("");
         primaryStage.setFullScreen(true);
